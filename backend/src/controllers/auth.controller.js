161f53cacca61838994c37e56e4a1794a917369b
wardah01 <wardahbatool2313@gmail.com>
2026-07-24 14:23:32 +0500

    feat(backend): add authentication routes and controllers

---
+++ backend/src/controllers/auth.controller.js
@@ -0,0 +1,10 @@
+function login(req, res) {
+  res.send("login endpoint working");
+}
+function signup(req, res) {
+  res.send("signup endpoint working");
+}
+function logout(req, res) {
+  res.send("logout endpoint working");
+}
+export { login, signup, logout };

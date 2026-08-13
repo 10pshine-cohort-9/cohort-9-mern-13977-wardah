b8d947a95b7d486e7355be372a1a835c55c445e2
wardah01 <wardahbatool2313@gmail.com>
2026-08-13 22:00:40 +0500

    fix(backend): address issues

--- backend/src/middleware/error.middleware.js
+++ backend/src/middleware/error.middleware.js
@@ -1,9 +1,11 @@
-import logger from "../utils/logger.js";
-
 const errorMiddleware = (err, req, res, next) => {
   logger.error({ err }, "Unhandled error occurred");
 
-  res.status(500).json({
+  if (res.headersSent) {
+    return next(err);
+  }
+
+  return res.status(500).json({
     message: "Internal Server Error",
   });
 };
